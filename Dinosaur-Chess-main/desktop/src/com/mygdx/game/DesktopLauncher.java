efe1639a9df9c1a4778c918fba25ff5651b850e1
Wesley Tu <wtu@scu.edu>
2023-11-11 17:05:08 -0800

    all

---
+++ Dinosaur-Chess-main/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.game.DinosaurChess;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setWindowedMode(1280,800);
+		config.useVsync(true);
+		config.setTitle("Dinosaur Chess");
+		new Lwjgl3Application(new DinosaurChess(), config);
+	}
+}
